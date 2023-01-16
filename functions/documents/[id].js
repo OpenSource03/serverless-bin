f213efaa74c6256db8ed791da93ac96b129f6757
OpenSource <opensource@thearcadia.xyz>
2023-01-16 17:49:11 +0100

    Finished GET endpoint

--- functions/documents/[id].js
+++ functions/documents/[id].js
@@ -1,8 +1,12 @@
 export function onRequest(context) {
+  const { id } = context.params;
+
+  const data = context.env.FILES_KV.get(id);
+
   return new Response(
     JSON.stringify({
       id: context.params.id,
-      data: "Test",
+      data,
     })
   );
 }
